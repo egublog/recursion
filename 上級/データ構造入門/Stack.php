62466b1b18814b80d2035e81db177bc0d41fd864
egublog <egukei0729@gmail.com>
2022-02-14 10:53:28 +0900

    [ADD] consecutiveWalkメソッド

--- 上級/データ構造入門/Stack.php
+++ 上級/データ構造入門/Stack.php
@@ -64,3 +64,25 @@ function reverse($arr)
 }
 $arr = [1,2,3,4,5,6];
 print(json_encode(reverse($arr))) .PHP_EOL;
+
+// 前の数字よりも連続で小さくなっている要素を探す
+function consecutiveWalk($arr)
+{
+  $stack = new Stack();
+  $stack->push($arr[0]);
+  for($i = 1; $i < count($arr); $i++){
+      if($stack->peek() < $arr[$i]){
+          while($stack->pop() != null);
+      }
+      $stack->push($arr[$i]);
+  }
+  $results = [];
+
+  while($stack->peek() != null) array_unshift($results, $stack->pop());
+  
+  return $results;
+}
+
+print(json_encode(consecutiveWalk([3,4,20,45,56,6,4,3,5,3,2]))) .PHP_EOL; // [5,3,2]
+print(json_encode(consecutiveWalk([4,5,4,2,4,3646,34,64,3,0,-34,-54]))) .PHP_EOL; // [64,3,0,-34,-54]
+print(json_encode(consecutiveWalk([4,5,4,2,4,3646,34,64,3,0,-34,-54,4]))) .PHP_EOL; // [4]
