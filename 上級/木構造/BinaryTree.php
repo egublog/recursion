f25ed884c0fbcf01dd0541cca442908ac4e1c342
egublog <egukei0729@gmail.com>
2022-02-15 23:23:16 +0900

    [ADD] 二分木のの探索

--- 上級/木構造/BinaryTree.php
+++ 上級/木構造/BinaryTree.php
@@ -26,3 +26,16 @@ $binaryTree->right = $node3;
 print("Root: " . $binaryTree->data . PHP_EOL);
 print("Left: " . $binaryTree->left->data . PHP_EOL);
 print("Right: " . $binaryTree->right->data . PHP_EOL);
+
+// 二分探索木
+// NOTE: 完全二分木の高さがlog2(n)であるという性質を持つ
+function linearSearch($key, $haystack){
+  for($i = 0; $i < count($haystack); $i++){
+      if($key === $haystack[$i]) return $i;
+  }
+  return -1;
+}
+
+$l1 = [3,4,2,5,46,23,3,55,67,24,65];
+echo(linearSearch(5, $l1)). PHP_EOL;
+echo(linearSearch(24, $l1)). PHP_EOL;
