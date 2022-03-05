13d439eda32697672f4544ade0582660a669fd6b
egublog <egukei0729@gmail.com>
2022-03-06 02:06:06 +0900

    [ADD] 他の型のリスト

--- 中級/リスト/List.php
+++ 中級/リスト/List.php
@@ -21,3 +21,18 @@ printIntArray($arr1);
 $arr1[3] = 34;
 $arr1[1] = 40;
 printIntArray($arr1);
+
+function printArray($arr){
+  for ($i = 0; $i < count($arr); $i++){
+      echo $arr[$i];
+  }
+  echo ''. PHP_EOL;
+}
+
+$doubleArr = [34.5,34.4,23,54.3];
+$charArr = ['h','e','l','l','o'];
+$stringArr = ["The race is starting.", "A dog just escaped", "The company ran out of business"];
+
+printArray($doubleArr);
+printArray($charArr);
+printArray($stringArr);
