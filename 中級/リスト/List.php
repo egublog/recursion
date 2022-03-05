b5f7f5f5436b6fb8a52eabdd569124b8bd6077d5
egublog <egukei0729@gmail.com>
2022-03-06 02:11:01 +0900

    [ADD] 配列(2)

--- 中級/リスト/List.php
+++ 中級/リスト/List.php
@@ -7,6 +7,7 @@ for($i = 0; $i < strlen($str); $i++) {
     echo $str[$i]. PHP_EOL;
 }
 
+// 配列(1)
 // 整数のリスト
 function printIntArray($intArr)
 {
@@ -36,3 +37,53 @@ $stringArr = ["The race is starting.", "A dog just escaped", "The company ran ou
 printArray($doubleArr);
 printArray($charArr);
 printArray($stringArr);
+
+
+// 配列(2)
+function isEven($n){
+  return $n % 2 == 0;
+}
+
+// 整数の配列を取り込んで、偶数が何個あるかを調べます。
+function totalEven($listOfInts){
+  $count = 0;
+  for($i = 0; $i < count($listOfInts); $i++){
+      $count += isEven($listOfInts[$i]) ? 1 : 0;
+  }
+  return $count;
+}
+
+$list1 = [3,43,5,4,2,100,6];
+
+$totalEvens = totalEven($list1);
+echo $totalEvens . PHP_EOL;
+
+
+// 例題1
+// 整数によって構成される配列が与えられるので、配列内の全ての要素を足し合わせた数値を返す、sumArrayElementという関数を作成してください。
+// [3,43,5,4,2,100,6] --> 163
+// [1,2,3,4,5,6] --> 21
+// [32,21,10,3,5,6] --> 77
+function sumArrayElement($arr){
+  $sum = 0;
+  for($i = 0; $i < count($arr); $i++){
+      $sum += $arr[$i];
+  }
+  return $sum;
+}
+
+
+// 例題2
+// 整数によって構成される配列が与えられるので、配列内の最大値を返す、maxValueという関数を作成してください。
+// [3,43,5,4,2,100,6] --> 100
+// [1,2,3,4,5,6] --> 6
+// [32,21,10,3,5,60,18,32,] --> 60
+function maxValue($arr){
+  $max = $arr[0];
+  for($i = 1; $i < count($arr); $i++){
+      if($max < $arr[$i]){
+          $max = $arr[$i];
+      }
+  }
+  return $max;
+}
