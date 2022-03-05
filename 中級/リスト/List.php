c28231434f02075a46316a9f894ab7aad8df762a
egublog <egukei0729@gmail.com>
2022-03-05 23:44:36 +0900

    [ADD] 整数のリスト

--- 中級/リスト/List.php
+++ 中級/リスト/List.php
@@ -6,3 +6,18 @@ $str = "Hello World!";
 for($i = 0; $i < strlen($str); $i++) {
     echo $str[$i]. PHP_EOL;
 }
+
+// 整数のリスト
+function printIntArray($intArr)
+{
+  for ($i = 0; $i < count($intArr) ; $i++){
+      echo $intArr[$i]. PHP_EOL;
+  }
+}
+
+$arr1 = [40,3,22,-2,4,8];
+printIntArray($arr1);
+
+$arr1[3] = 34;
+$arr1[1] = 40;
+printIntArray($arr1);
