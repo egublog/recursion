a8c17c0ee270885a010cc662763753331b9b3f76
egublog <egukei0729@gmail.com>
2022-03-20 23:43:14 +0900

    [ADD] 動的配列(3)

--- 中級/リスト/DynamicArray.php
+++ 中級/リスト/DynamicArray.php
@@ -31,3 +31,77 @@ array_push($dArr, 543);
 $dArr[] = -23;
 
 printArray($dArr);
+
+// 動的配列(3)
+function printArray3($intArr){
+    $outputString = "[";
+    for ($i = 0; $i < count($intArr); $i++) {
+        $outputString .= $intArr[$i] . " ";
+    }
+    echo $outputString . "]" . PHP_EOL;
+}
+
+// 初期配列を2つの項目に設定します。
+$dArr = [2,3];
+
+echo "Insert/Delete at the beginning O(n)" . PHP_EOL;
+printArray3($dArr);
+
+// 配列の先頭に要素を追加します。O(n)
+//array_unshift(配列,値)関数を使います。
+array_unshift($dArr,3);
+//引数はいくつでも設定可能です。
+array_unshift($dArr,3,43,5234,34);
+printArray3($dArr);
+
+// 最初の要素を削除します。O(n)
+//array_shift(配列)関数を使います。
+array_shift($dArr);
+printArray3($dArr);
+
+// インデックス2の位置にある要素を削除します。O(n)
+//array_splice関数を使います。array_splice(配列、スタート、削除したい数、追加したい値)
+array_splice($dArr,2, 1);
+printArray3($dArr);
+
+array_unshift($dArr,343,343,232,12,23,-23,10);
+printArray3($dArr);
+
+// 配列の途中に要素を追加します。
+echo "Insert/Delete at the middle O(n)" . PHP_EOL;
+array_splice($dArr, floor(count($dArr) / 2), 0, 4);
+printArray3($dArr);
+//array_splice()で配列のデータを追加できます。
+array_splice($dArr, floor(count($dArr) / 2), 0,[343,32,23,24,12,23,98,767]);
+printArray3($dArr);
+
+// 配列の途中の要素を削除します。
+array_splice($dArr, floor(count($dArr) / 2), 1);
+// 配列の途中から、5つ先まで削除します。
+array_splice($dArr, floor(count($dArr) / 2), 5);
+printArray3($dArr);
+
+echo "Insert/Delete at the end O(1)" . PHP_EOL;
+
+// 配列の最後に要素を追加します。
+// 1つの要素をプッシュします。
+$dArr[] = 4;
+array_push($dArr, 50);
+printArray3($dArr);
+
+// 配列の最後に複数要素を追加します。いくつでも可能です。
+array_push($dArr, 6,3,4,54);
+printArray3($dArr);
+
+// 配列の最後を削除します。
+// 1つの要素を削除します。
+array_pop($dArr);
+printArray3($dArr);
+
+echo "Pop 5 from end " . PHP_EOL;
+
+// 後ろから5つの要素を削除します。
+for($i = 0; $i < 5; $i++) {
+    array_pop($dArr);
+}
+printArray3($dArr);
