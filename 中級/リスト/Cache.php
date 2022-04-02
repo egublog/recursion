6b1fa96421a0b16ae8cc73733e5ce68759f38b8a
egublog <egukei0729@gmail.com>
2022-04-02 23:49:20 +0900

    [ADD] ハッシュマップキャッシング(1)

--- 中級/リスト/Cache.php
+++ 中級/リスト/Cache.php
@@ -113,3 +113,22 @@ function fibonacciNumberTail($n){
 }
 
 echo fibonacciNumberTail(20). PHP_EOL;
+
+// ハッシュマップキャッシング(1)
+function existsWithinList($listL,$dataY){
+  $hashMap = array();
+
+  for($i = 0; $i < count($listL);$i++){
+      $hashMap[$listL[$i]] = $listL[$i];
+  }
+  return is_null($hashMap[$dataY]) ? false : true;
+}
+
+function printBool($bool){
+  echo $bool === true ? "True" : "False" .PHP_EOL ;
+}
+
+$sampleList = [3,10,23,3,4,50,2,3,4,18,6,1,-2];
+echo printBool(existsWithinList($sampleList,23)) .PHP_EOL;
+echo printBool(existsWithinList($sampleList,-2)) .PHP_EOL;
+echo printBool(existsWithinList($sampleList,100)) .PHP_EOL;
