959718971c129479b8c1f61a30377930f4285c4c
egublog <egukei0729@gmail.com>
2022-04-11 14:46:55 +0900

    [CREATE] クラス設計(3)

---
+++ OOP/オブジェクト指向設計/クラス設計(3).java
@@ -0,0 +1,108 @@
+class RGB24{
+  public int red;
+  public int green;
+  public int blue;
+
+  public RGB24(int red, int green, int blue){
+      this.red = red;
+      this.green = green;
+      this.blue = blue;
+  }
+
+  // 文字列のみを入力とするRGB24用のコンストラクタ。これには関数のオーバーロードを使用しています。
+  // コンストラクタは、新しいクラスのインスタンスを作成するときに最初に実行しなければならない関数であることを覚えておいてください。ここでは、状態を初期化するために必要なことは何でもできます。
+  public RGB24(String inputString){
+      int l = inputString.length();
+
+      if(l == 6) this.setColorsByHex(inputString);
+      else if(l == 24) this.setColorsByBin(inputString);
+      else this.setAsBlack();
+  }
+
+  // RGBの状態を16進数で指定します。
+  public void setColorsByHex(String hex){
+      if(hex.length() != 6) this.setAsBlack();
+      else{
+          this.red = Integer.parseInt(hex.substring(0,2), 16);
+          this.green = Integer.parseInt(hex.substring(2,4), 16);
+          this.blue = Integer.parseInt(hex.substring(4,6), 16);
+      }
+  }
+
+  // RGBの状態を2進数で指定します。
+  public void setColorsByBin(String bin){
+      if(bin.length() != 24) this.setAsBlack();
+      else{
+          this.red = Integer.parseInt(bin.substring(0,8), 2);
+          this.green = Integer.parseInt(bin.substring(8,16), 2);
+          this.blue = Integer.parseInt(bin.substring(16), 2);
+      }
+  }
+
+  // 状態を黒に設定します。
+  public void setAsBlack(){
+      this.red = 0;
+      this.green = 0;
+      this.blue = 0;
+  }
+
+  public String getHex(){
+      String hex = Integer.toHexString(this.red);
+      hex+=Integer.toHexString(this.green);
+      hex+=Integer.toHexString(this.blue);
+
+      return hex;
+  } 
+
+  public String getBits(){
+      return Integer.toBinaryString(Integer.parseInt(this.getHex(), 16));
+  }
+
+  // RGBが赤、青、緑の中でどの色が濃いのか、もしくはgrayなのか決定することができます。
+  public String getColorShade(){
+      if(this.red == this.green && this.green == this.blue) return "grayscale";
+      String[] stringTable = new String[]{"red","green","blue"};
+      int[] values = {this.red, this.green, this.blue};
+
+      // max algorithm。maxへの線形探索。
+      int max = values[0];
+      int maxIndex = 0;
+      for(int i = 1; i < values.length; i++){
+          if(max <= values[i]){
+              max = values[i];
+              maxIndex = i;
+          }
+      }
+
+      return stringTable[maxIndex];
+  }
+
+  // 文字列変換メソッドtoString()。Javaは、System.out.println(data)関数のように、オブジェクトを文字列に変換する必要がある場合、この関数を自動的に呼び出します。
+  public String toString(){
+      return "The color is rgb(" + this.red + "," + this.green + "," + this.blue + "). Hex: " + this.getHex() + ", binary: " + this.getBits();
+  }
+}
+
+class Main{
+  public static void main(String[] args){
+      RGB24 color1 = new RGB24(0, 153, 255);
+      RGB24 color2 = new RGB24("ff99cc"); //rgb(255, 153, 204)
+      RGB24 color3 = new RGB24("100110011111111100110011"); //rgb(153, 255, 51)
+      RGB24 gray = new RGB24("7b7b7b"); //rgb(123, 123, 123)
+
+      System.out.println(color1);
+      System.out.println(color2);
+      System.out.println(color3);
+      System.out.println(gray);
+      
+      System.out.println();	
+      System.out.println("Changing the state of colors");
+      System.out.println();
+      
+      // 状態の変更
+      gray.setAsBlack();
+      System.out.println(gray);
+      color1.setColorsByHex("2EB656");
+      System.out.println(color1);
+  }
+}
