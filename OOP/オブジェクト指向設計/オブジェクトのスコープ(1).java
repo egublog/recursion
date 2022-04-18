cc9cdb4e28b92a35f1ca8e02697f8ee70d779b36
egublog <egukei0729@gmail.com>
2022-04-18 23:57:57 +0900

    [CREATE] オブジェクトのスコープ(1)

---
+++ OOP/オブジェクト指向設計/オブジェクトのスコープ(1).java
@@ -0,0 +1,60 @@
+class Wallet{
+  public int bill1;
+  public int bill5;
+  public int bill10;
+  public int bill20;
+  public int bill50;
+  public int bill100;
+
+  public Wallet(){}
+
+  public int getTotalMoney(){
+      return (1*this.bill1) + (5*this.bill5) + (10*this.bill10) + (20*this.bill20) + (50*this.bill50) + (100*this.bill100);
+  }
+}
+
+class Person{
+  public String firstName;
+  public String lastName;
+  public int age;
+  public double heightM;
+  public double weightKg;
+  public Wallet wallet;
+
+  public Person(String firstName){
+      this.firstName = firstName;
+  }
+
+  public int getCash(){
+      // オブジェクト型なので、walletはnullになります。
+      if(this.wallet == null){
+          System.out.println("NO WALLET");
+          return 0;
+      }
+      return this.wallet.getTotalMoney();
+  }
+}
+
+class Main{
+  public static void main(String[] args){
+      Person p = new Person("Ryu"); 
+      System.out.println("firstname - " + p.firstName);
+      System.out.println("lastname - " + p.lastName); //Stringはオブジェクト型で、デフォルトはnullになります。
+      System.out.println("age - " + p.age);
+      System.out.println("height - " + p.heightM);
+      System.out.println("weight - " + p.weightKg);
+      System.out.println("Current Money - " + p.getCash());
+
+      p.lastName = "Poolhopper";
+      p.age = 40;
+      p.heightM = 180;
+      p.weightKg = 140;
+
+      System.out.println();
+      System.out.println("firstname - " + p.firstName);
+      System.out.println("lastname - " + p.lastName);
+      System.out.println("age - " + p.age);
+      System.out.println("height - " + p.heightM);
+      System.out.println("weight - " + p.weightKg);
+  }
+}
